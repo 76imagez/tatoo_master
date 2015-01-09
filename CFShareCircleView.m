db229991f740851fe3ba52a1b8f96e15e70355b4
lys2907 <teqwin@Teqwins-MacBook-Pro.local>
2015-01-09 11:31:39 +0800

    Revert cf8bc7f..ba7e029
    This rolls back to commit cf8bc7f7919e5c46baffb5d6cd19132d4aabd02f.

--- CFShareCircleView.m
+++ CFShareCircleView.m
@@ -46,7 +46,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         _sharers = [[NSMutableArray alloc] initWithObjects: [CFSharer whatsapp],[CFSharer more],[CFSharer sina_weibo],[CFSharer twitter],[CFSharer facebook],nil];
-        
+
         [self setUpCircleLayers];
     }
     return self;
@@ -73,14 +73,14 @@
     }
     if(_sharingOptionsIsVisible)
         _sharingOptionsView.frame = self.bounds;
-    
+        
     [self updateLayers];
 }
 
 #pragma mark -
 #pragma mark - Private methods
 
-- (void)setUpCircleLayers {
+- (void)setUpCircleLayers {  
     // Set all the defaults for the share circle.
     _imageLayers = [[NSMutableArray alloc] init];
     self.hidden = YES;
@@ -229,7 +229,7 @@
     CGRect rect = CGRectMake(0.0f, 0.0f, 45.0f, 45.0f);
     UIImage *closeButtonImage = [UIImage imageNamed:@"close.png"];
     UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0.0f);
-    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextRef context = UIGraphicsGetCurrentContext();    
     CGContextSetFillColorWithColor(context, [[UIColor colorWithRed:35.0/255.0 green:35.0/255.0 blue:35.0/255.0 alpha:1.0] CGColor]);
     CGContextFillRect(context, rect);
     [closeButtonImage drawInRect:CGRectMake(15.0f,15.0f,closeButtonImage.size.width,closeButtonImage.size.height) blendMode:kCGBlendModeNormal alpha:1.0];
@@ -321,7 +321,7 @@
             float y = layer.position.y - sinf(trig)*PATH_SIZE/2.0;
             [_sharerLocations addObject:[NSValue valueWithCGPoint:CGPointMake(x, y)]];
             layer.position = [[_sharerLocations objectAtIndex:i] CGPointValue];
-        }
+        }  
     } else {
         for(int i = 0; i < _numberSharersInCircle; i++) {
             // Animate the base layer for the main rotation.
@@ -428,7 +428,7 @@
     CGContextSetFillColorWithColor(context, [[UIColor whiteColor] CGColor]);
     CGContextFillRect(context, rect);
     UIImage *tempImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+    UIGraphicsEndImageContext();    
     return tempImage;
 }
 
@@ -541,7 +541,7 @@
         imageView = [[UIImageView alloc] initWithFrame:CGRectMake(30.0, 15.0, 30.0, 30.0)];
         imageView.tag = IMAGE_VIEW_TAG;
         [cell.contentView addSubview:imageView];
-    }
+    }   
     
     // Set the label and image properties.
     nameLabel.text = sharer.name;
